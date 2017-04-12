5129429c813b907497279a902eca01a947840495
sykim <sykim.finshot@gmail.com>
2017-04-12 19:32:11 +0900

    마이너수정

--- app/src/main/java/com/example/jobs/solveandroid/dialog/VariableDialog.java
+++ app/src/main/java/com/example/jobs/solveandroid/dialog/VariableDialog.java
@@ -2,11 +2,17 @@ package com.example.jobs.solveandroid.dialog;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AlertDialog;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.jobs.solveandroid.R;
 import com.example.jobs.solveandroid.editor.Type;
@@ -24,15 +30,26 @@ public class VariableDialog {
                 .setView(createView(context));
     }
 
-    private View createView(Context context) {
+    private View createView(final Context context) {
         View root = LayoutInflater.from(context)
                 .inflate(R.layout.dialog_create_variable, null);
         Spinner spinner = (Spinner) root.findViewById(R.id.spinner);
-        spinner.setAdapter(new ArrayAdapter<>(
+//        http://www.broculos.net/2013/09/how-to-change-spinner-text-size-color.html
+        spinner.setAdapter(new ArrayAdapter<Type>(
                 context,
                 android.R.layout.simple_list_item_1,
                 Type.values()
-        ));
+        ) {
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View root = super.getView(position, convertView, parent);
+                TextView textView = (TextView) root.findViewById(android.R.id.text1);
+//                textView.setTextColor(0xffffff);
+//                textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+                return root;
+            }
+        });
         return root;
     }
 
